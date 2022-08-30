600432337c8baed8855d4903eb8b7415bc679e9a
Yasin Torun <yasintorun4680@gmail.com>
2022-08-30 14:05:12 +0300

    Changed namespace -> Added Prefix: Kod

--- src/WebAPI/Configs/ApplicationConfigurations.cs
+++ src/WebAPI/Configs/ApplicationConfigurations.cs
@@ -1,19 +1,17 @@
 ﻿using Microsoft.AspNetCore.Rewrite;
 
-namespace WebAPI.Configs
+namespace Kod.WebAPI.Configs;
+public static class ApplicationConfigurations
 {
-    public static class ApplicationConfigurations
+    public static void ApplyConfiguration(this IApplicationBuilder builder)
     {
-        public static void ApplyConfiguration(this IApplicationBuilder builder)
-        {
-            builder.UseAuthentication();
-            builder.UseAuthorization();
-            builder.UseRouting();
-            builder.UseSwagger();
-            builder.UseSwaggerUI();
-            builder.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
-            builder.UseCors("CorsPolicity");
-            builder.UseHealthChecks("/healthapp");
-        }
+        builder.UseAuthentication();
+        builder.UseAuthorization();
+        builder.UseRouting();
+        builder.UseSwagger();
+        builder.UseSwaggerUI();
+        builder.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
+        builder.UseCors("CorsPolicity");
+        builder.UseHealthChecks("/healthapp");
     }
 }
