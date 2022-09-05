ab3858abdd75d3079d4961f6be13dc3d244bf1b5
Yasin Torun <yasintorun4680@gmail.com>
2022-09-05 21:04:06 +0300

    Create Student.cs

---
+++ src/Domain/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Core.Domain.Models;
+
+namespace LMS.Domain.Models
+{
+    public class Student : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Identity { get; set; }
+    }
+}
