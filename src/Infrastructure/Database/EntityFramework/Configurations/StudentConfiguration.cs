508f5cb0b9d2816ad281274649cf9906e69e9f34
Yasin Torun <yasintorun4680@gmail.com>
2022-09-05 21:04:01 +0300

    Create database infrastructure

---
+++ src/Infrastructure/Database/EntityFramework/Configurations/StudentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Database.EntityFramework.Configurations;
+
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Identity).HasMaxLength(11).IsRequired();
+        builder.Property(x => x.FirstName).HasMaxLength(30).IsRequired();
+        builder.Property(x => x.LastName).HasMaxLength(30).IsRequired();
+    }
+}
---
+++ src/Infrastructure/Database/EntityFramework/Contexts/LMSContext.cs
@@ -0,0 +1,17 @@
+﻿using LMS.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace LMS.Infrastructure.Database.EntityFramework.Contexts
+{
+    public class LMSContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
