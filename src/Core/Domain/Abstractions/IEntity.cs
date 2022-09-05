5dab50a8999670fa84255da8b9b8e5bf936dd2d2
Yasin Torun <yasintorun4680@gmail.com>
2022-09-05 20:20:05 +0300

    Created Entity models on the Core layer

---
+++ src/Core/Domain/Abstractions/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Kod.Core.Domain.Abstractions
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ src/Core/Domain/Models/Entity.cs
@@ -0,0 +1,7 @@
+﻿using Kod.Core.Domain.Abstractions;
+
+namespace Kod.Core.Domain.Models;
+public abstract class Entity : IEntity
+{
+    public int Id { get; set ; }
+}
