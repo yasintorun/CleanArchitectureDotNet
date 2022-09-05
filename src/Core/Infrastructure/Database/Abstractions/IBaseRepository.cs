f16e01b914ea824f1770e7046f623c899aba1423
Yasin Torun <yasintorun4680@gmail.com>
2022-09-05 22:40:49 +0300

    First shot test database

--- src/Core/Infrastructure/Database/Abstractions/IBaseRepository.cs
+++ src/Core/Infrastructure/Database/Abstractions/IBaseRepository.cs
@@ -4,7 +4,7 @@ namespace LMS.Core.Infrastructure.Database.Abstractions
 {
     public interface IBaseRepository<T>
     {
-        IEnumerable<T> GetList(Expression<Func<T, bool>>? predicate);
+        IEnumerable<T> GetList(Expression<Func<T, bool>>? predicate=null);
         T? Get(Expression<Func<T, bool>> predicate);
         T Add(T entity);
         T Update(T entity);
---
+++ src/WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using LMS.Application.Abstractions.Repositories;
+using LMS.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentController(ILogger<StudentController> logger, IStudentRepository studentRepository)
+        {
+            _logger = logger;
+            _studentRepository = studentRepository;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetStudents()
+        {
+            var list = _studentRepository.GetList();
+            return Ok(list);
+        }
+
+
+        [HttpPost("")]
+        public IActionResult AddStudents(Student student)
+        {
+            var addedStudent = _studentRepository.Add(student);
+            return Ok(addedStudent);
+        }
+    }
+}
